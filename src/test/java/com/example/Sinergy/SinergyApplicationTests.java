a100a771ce46e579a63105460067c1c1ae9a5cf8
GScabuzzi <gustavo-gsg@hotmail.com>
2021-09-28 15:54:55 -0300

    [Feature Gustavo - Task: Created Spring Project]

---
+++ src/test/java/com/example/Sinergy/SinergyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sinergy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinergyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
