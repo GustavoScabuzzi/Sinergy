c299eb1c24d6a5cfe3db1f96d637d23ff69127b9
GScabuzzi <gustavo-gsg@hotmail.com>
2021-09-29 15:14:23 -0300

    [Feature Gustavo - Task: Add Class PostagemController and methods CRUD]

--- src/main/java/com/sinergy/controller/PostagemController.java
+++ src/main/java/com/sinergy/controller/PostagemController.java
@@ -1,25 +1,77 @@
 package com.sinergy.controller;
+/**
+ * Classe Controlador com metodos CRUD
+ * 
+ * @author gustavo
+ * 
+ */
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sinergy.model.Postagem;
+import com.sinergy.repository.RepositorioPostagem;
+
 @RestController
 @RequestMapping("/sinergy/posts")
-@CrossOrigin("*")
+@CrossOrigin(allowedHeaders = "*", origins = "*")
 public class PostagemController {
 
 	@Autowired
-	private PostagemRepository repository;
-	
-	@GetMapping
-	public ResponseEntity<List<Postagem>> GetAll(){
-		return ResponseEntity.ok(repository.finAll());
+	private RepositorioPostagem repository;
+
+	@GetMapping("/allPosts")
+	public ResponseEntity<List<Postagem>> GetAll() {
+		return ResponseEntity.ok(repository.findAll());
+	}
+
+	@GetMapping("/id/{id}")
+	public ResponseEntity<Postagem> GetById(@PathVariable long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
 	}
 	
-	@GetMapping
-	public ResponseEntity
+	@GetMapping("/legenda/{legenda}")
+	public ResponseEntity<List<Postagem>> GetByLegenda(@PathVariable String legenda){
+		return ResponseEntity.ok(repository.findAllByLegendaContainingIgnoreCase(legenda));
+	}
 	
+	@GetMapping("/editado/{editado}")
+	public ResponseEntity<List<Postagem>> GetByEditado(@PathVariable Boolean editado){
+		return ResponseEntity.status(201).body(repository.findAllByEditadoContainingIgnoreCase(editado));
+	}
+	
+	@PostMapping("/salvar")
+	public ResponseEntity<Postagem> salvar(@Valid @RequestBody Postagem novaPostagem){
+		return ResponseEntity.status(201).body(repository.save(novaPostagem));
+	}
+	
+	@PutMapping("/atualizar")
+	public ResponseEntity<Postagem> atualizar(@Valid @RequestBody Postagem atualizarPostagem){
+		return ResponseEntity.status(201).body(repository.save(atualizarPostagem));
+	}
+	
+	@DeleteMapping("/deletar/{id}")
+	public ResponseEntity<Postagem> deletar(@PathVariable(value = "id") Long idPostagem){
+		Optional<Postagem> objetoOptional = repository.findById(idPostagem);
+		
+		if (objetoOptional.isPresent()) {
+			repository.deleteById(idPostagem);
+			return ResponseEntity.status(204).build();
+		} else {
+			return ResponseEntity.status(400).build();
+		}
+	}
 }
--- src/main/java/com/sinergy/repository/RepositorioPostagem.java
+++ src/main/java/com/sinergy/repository/RepositorioPostagem.java
@@ -3,7 +3,6 @@ package com.sinergy.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import com.sinergy.model.Postagem;
 
 public interface RepositorioPostagem extends JpaRepository<Postagem, Long> {
