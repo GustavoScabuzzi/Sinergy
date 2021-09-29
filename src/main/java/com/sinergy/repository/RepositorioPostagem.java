6a2edf5efbee63c245bacd98bfe2574f3e135756
GScabuzzi <gustavo-gsg@hotmail.com>
2021-09-29 13:47:44 -0300

    [Feature Gustavo - Task: ADD methods postagemRepository]

--- src/main/java/com/sinergy/repository/RepositorioPostagem.java
+++ src/main/java/com/sinergy/repository/RepositorioPostagem.java
@@ -1,9 +1,31 @@
 package com.sinergy.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.sinergy.model.Postagem;
 
 public interface RepositorioPostagem extends JpaRepository<Postagem, Long> {
 
+	/**
+	 * Metodo utilizado para realizar pesquisa pela coluna legenda da tabela Postagem
+	 * 
+	 * @param legenda
+	 * @return lista com as Postagens a partir da palavra digitada
+	 * @author Gustavo
+	 * @since 1.0
+	 */
+	public List<Postagem> findAllByLegendaContainingIgnoreCase(String titulo);
+	
+	/**
+	 * Metodo utilizado para realizar pesquisa pela coluna Editado da tabela Postagem
+	 * 
+	 * @param editado
+	 * @return lista com as Postagens a partir do valor true ou false
+	 * @author Gustavo
+	 * @since 1.0
+	 */
+	public List<Postagem> findAllByEditadoContainingIgnoreCase(Boolean editado);
+	
 }
