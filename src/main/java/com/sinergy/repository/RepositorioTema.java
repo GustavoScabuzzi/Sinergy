e870c4d79193de448fd2653f2583e53133fc4cc4
Gustavo Scabuzzi Gonçalves <34173963+GustavoScabuzzi@users.noreply.github.com>
2021-10-01 15:48:37 -0300

    Revert "Merge pull request #6 from GustavoScabuzzi/revert-4-desenvolvimento"
    This reverts commit fde3ced1db31ffe0ff353a8fa6e49f6c863651c4, reversing
    changes made to e65be345af787dbd97b3730cc4af7bd8d6ff3b34.

---
+++ src/main/java/com/sinergy/repository/RepositorioTema.java
@@ -0,0 +1,16 @@
+package com.sinergy.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sinergy.model.Tema;
+
+public interface RepositorioTema extends JpaRepository<Tema, Long> {
+	
+	public List<Tema> findAllByDoacaoContainingIgnoreCase(Boolean doacao);
+	public List<Tema> findAllByVoluntarioContainingIgnoreCase(Boolean voluntario);
+	public List<Tema> findAllByInformativoContainingIgnoreCase(Boolean informativo);
+	public List<Tema> findAllByAnyContainingIgnoreCase(Boolean any);
+	
+}
