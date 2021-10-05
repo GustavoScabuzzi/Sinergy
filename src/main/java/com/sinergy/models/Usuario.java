7e4c0c093878b4198d399260546a342dcabbc9be
Gustavo Scabuzzi Gonçalves <34173963+GustavoScabuzzi@users.noreply.github.com>
2021-10-05 17:50:47 -0300

    [Feature Gustavo - Task: Verificaçao model Usuario]

--- src/main/java/com/sinergy/models/Usuario.java
+++ src/main/java/com/sinergy/models/Usuario.java
@@ -9,6 +9,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -16,14 +17,15 @@ import javax.validation.constraints.Size;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 /**
- * Classe espelho da tabela usuario no banco db_blogpessoal.
+ * Classe espelho da tabela usuario no banco db_sinergy.
  * 
- * @author Turma34
+ * @author Gustavo
  * @since 1.0
  * 
  */
 
 @Entity
+@Table(name = "usuario")
 public class Usuario {
 
 	private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long idUsuario;
@@ -31,9 +33,9 @@ public class Usuario {
 	private @Email String email;
 	private @NotBlank @Size(min = 5, max = 100) String senha;
 
-	// @OneToMany(mappedBy = "criador", cascade = CascadeType.REMOVE)
-	// @JsonIgnoreProperties({"criador"})
-	// private List<Postagem> minhasPostagens = new ArrayList<>();
+	@OneToMany(mappedBy = "criador", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties({"criador"})
+	private List<Postagem> minhasPostagens = new ArrayList<>();
 
 	public Long getIdUsuario() {
 		return idUsuario;
