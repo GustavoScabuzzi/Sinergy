8a4a5de9f7639fd62ce0d2362d4d4e0f3b8ff15b
George <george_roots@hotmail.com>
2021-10-13 11:43:29 -0300

    [Feature George - Task: ADD class CredentialsDTO]

--- src/main/java/com/sinergy/modelsDTOs/CredentialsDTO.java
+++ src/main/java/com/sinergy/modelsDTOs/CredentialsDTO.java
@@ -1,5 +1,56 @@
 package com.sinergy.modelsDTOs;
 
+/**
+ * @author George
+ * @since 1.0
+ *
+ */
+
 public class CredentialsDTO {
+	private Long idUsuario;
+	private String nome;
+	private String email;
+	private String senha;
+	private String token;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Long getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(Long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
 
 }
