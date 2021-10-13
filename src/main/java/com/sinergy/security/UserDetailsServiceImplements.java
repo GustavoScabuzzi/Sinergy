57d1f3c9019381b4669ebcded6a22af5e36be4f3
Drope <papagodao.gota@gmail.com>
2021-10-13 15:00:36 -0300

    [Feature Pedro - Task: ADD class UserDetailsServiceImplements]

--- src/main/java/com/sinergy/security/UserDetailsServiceImplements.java
+++ src/main/java/com/sinergy/security/UserDetailsServiceImplements.java
@@ -1,5 +1,41 @@
 package com.sinergy.security;
 
-public class UserDetailsServiceImplements {
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.sinergy.models.Usuario;
+import com.sinergy.repositories.UsuarioRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.sinergy.models.Usuario;
+import com.sinergy.repositories.UsuarioRepository;
+
+@Service
+public class UserDetailsServiceImplements implements UserDetailsService {
+	
+	private @Autowired UsuarioRepository repository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		Optional<Usuario> objetoOptional = repository.findByEmail(username);
+		
+		if (objetoOptional.isPresent()) {
+			return new UserDetailsImplements(objetoOptional.get());
+		} else {
+			throw new UsernameNotFoundException(username + " Não Consta");
+		}
+		
+	}
 
 }
