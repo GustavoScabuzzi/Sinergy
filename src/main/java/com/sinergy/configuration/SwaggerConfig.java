bb66f55b0f6527daafe81750bd67c4500958bec1
Pablo <bruli_pablo@hotmail.com>
2021-10-18 10:24:45 -0300

    [Feature Pablo Task: Fazendo a configuração do Swagger]

---
+++ src/main/java/com/sinergy/configuration/SwaggerConfig.java
@@ -0,0 +1,56 @@
+package com.sinergy.configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Response;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.sinergy.controller"))
+				.paths(PathSelectors.any()).build().apiInfo(metadata()).useDefaultResponseMessages(false);
+	}			
+
+	public static ApiInfo metadata() {
+		return new ApiInfoBuilder()
+				.title("API - Sinergy")
+				.description("Projeto API Spring - Sinergy")
+				.version("1.0.0")
+				.license("Apache License Version 2.0")
+				.licenseUrl("https://github.com/GustavoScabuzzi/Sinergy")
+				.contact(contact()).build();
+	}
+
+	private static Contact contact() {
+		return new Contact("Gustavo Scabuzzi", "https://github.com/GustavoScabuzzi/Sinergy", "Gustavo@hotmail.com");
+	}
+
+	@SuppressWarnings("unused")
+	private static List<Response> responseMessage() {
+		return new ArrayList<Response>() {
+			private static final long serialVersionUID = 1L;
+			{
+				add(new ResponseBuilder().code("200").description("Sucesso!").build());
+				add(new ResponseBuilder().code("201").description("Criado!").build());
+				add(new ResponseBuilder().code("400").description("Erro na requisição!").build());
+				add(new ResponseBuilder().code("401").description("Não Autorizado!").build());
+				add(new ResponseBuilder().code("403").description("Proibido!").build());
+				add(new ResponseBuilder().code("404").description("Não Encontrado!").build());
+				add(new ResponseBuilder().code("500").description("Erro!").build());
+			}
+		};
+	}
+}
+
