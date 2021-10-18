98575b59f3154cdd170c71bf4474341bc6dd9bae
DanielSanm <danielelias3221@gmail.com>
2021-10-18 11:28:07 -0300

    Feature Daniel - Task: alterando configurações de segurança para testar endpoints

--- src/main/java/com/sinergy/security/BasicSecurityConfig.java
+++ src/main/java/com/sinergy/security/BasicSecurityConfig.java
@@ -13,36 +13,43 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	private @Autowired UserDetailsServiceImplements service;
-	
+
 	@Bean
 	public PasswordEncoder senhaEncoder() {
 		return new BCryptPasswordEncoder();
-		
+
 	}
-	
+
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/v1/usuario/salvar").permitAll()
+//				.antMatchers(HttpMethod.PUT, "/api/v1/usuario/credenciais").permitAll().anyRequest().authenticated()
+//				.and().httpBasic().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//				.and().cors().and().csrf().disable();
+//
+//	}
+
 	@Override
- 	protected void configure(HttpSecurity http) throws Exception {
-		http
-		.authorizeRequests()
-			.antMatchers(HttpMethod.POST, "/api/v1/usuario/salvar").permitAll()
-			.antMatchers(HttpMethod.PUT, "/api/v1/usuario/credenciais").permitAll()
-		.anyRequest().authenticated()
-		.and().httpBasic()
-		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and().cors()
-		.and().csrf().disable();
- 		
- 	}
-	
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/api/v1/usuario/salvar").permitAll().antMatchers("/api/v1/usuario/credenciais")
+				.permitAll().antMatchers(HttpMethod.OPTIONS).permitAll().anyRequest().authenticated().and().httpBasic()
+				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().cors().and()
+				.csrf().disable();
+	}
+
+//	@Override
+// 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.userDetailsService(service);
+//		
+//		auth.inMemoryAuthentication()
+//			.withUser("usuario").password(senhaEncoder().encode("senha")).authorities("ROLE_ADMIN");
+// 	}
+
 	@Override
- 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(service);
-		
-		auth.inMemoryAuthentication()
-			.withUser("usuario").password(senhaEncoder().encode("senha")).authorities("ROLE_ADMIN");
- 	}
-
+		auth.inMemoryAuthentication().withUser("root").password(senhaEncoder().encode("root")).authorities("ROLE_USER");
+	}
 }
-
