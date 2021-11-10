1144348eb84e42b2dc337dd99351809ffb7dd4c6
Gustavo Scabuzzi Gonçalves <34173963+GustavoScabuzzi@users.noreply.github.com>
2021-11-09 23:57:28 -0300

    [Feature Gustavo - Task: ADD campo foto e tipo na model usuario]

--- src/main/java/com/sinergy/modelsDTOs/CredentialsDTO.java
+++ src/main/java/com/sinergy/modelsDTOs/CredentialsDTO.java
@@ -17,6 +17,10 @@ public class CredentialsDTO {
 	private String senha;
 	
 	private String token;
+	
+	private String foto;
+	
+	private String tipo;
 
 	public String getEmail() {
 		return email;
@@ -56,6 +60,22 @@ public class CredentialsDTO {
 
 	public void setToken(String token) {
 		this.token = token;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
 	}
 
 }
